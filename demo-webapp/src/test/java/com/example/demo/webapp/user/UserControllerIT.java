bf198b0ccbf37bc42d3ea1335b826af907670ef1
Olivier De Guise <olivier.de-guise.1@ens.etsmtl.ca>
2021-09-27 23:27:04 -0400

    add

---
+++ demo-webapp/src/test/java/com/example/demo/webapp/user/UserControllerIT.java
@@ -0,0 +1,31 @@
+package com.example.demo.webapp.user;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerIT {
+
+    @Autowired
+    public MockMvc mockMvc;
+    
+    @Test
+    public void testGetUsers() throws Exception {
+        mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(containsString("<html>")));
+    }
+}
